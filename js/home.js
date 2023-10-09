6e0279a7f43903a88e991a104e942e7b03b16bbc
bruno <brno20045@gmail.com>
2023-10-09 20:04:36 -0300

    primeiro commit

---
+++ js/home.js
@@ -0,0 +1,21 @@
+
+const loginButton = document.getElementById('loginButton');
+
+
+loginButton.addEventListener('click', function() {
+   
+    window.location.href = 'login.html';
+});
+
+
+const ListaButton = document.getElementById('ListaButton');
+
+
+ListaButton.addEventListener('click', function() {
+   
+    window.location.href = 'ListaDePsicologos.html';
+});
+
+
+
+
